0d81a7acabe1871b7eef7b179130ee9a5861f9c9
baileyfrye1 <baileyafrye@comcast.net>
2026-09-22 20:26:49 -0500

    Updated astro config and added fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Poppins",
+    cssVariable: "--font-poppins",
+    options: {
+      variants: [
+        {
+          src: ['./src/assets/fonts/Poppins-Regular.woff2'],
+          weight: 'normal',
+          style: 'normal',
+        },
+        {
+          src: ['./src/assets/fonts/Poppins-Medium.woff2'],
+          weight: 'medium',
+          style: 'normal',
+        },
+        {
+          src: ['./src/assets/fonts/Poppins-Bold.woff2'],
+          weight: 'bold',
+          style: 'normal',
+        },
+        {
+          src: ['./src/assets/fonts/Poppins-ExtraBold.woff2'],
+          weight: 'extra bold',
+          style: 'normal',
+        },
+      ]
+    }
+  }, {
+    provider: fontProviders.local(),
+    name: "Inter",
+    cssVariable: "--font-inter",
+    options: {
+      variants: [
+        {
+          src: ['./src/assets/fonts/Inter-Variable-400-700.woff2'],
+          weight: '400 700',
+          style: 'normal',
+        }
+      ]
+    }
+  }]
 });
